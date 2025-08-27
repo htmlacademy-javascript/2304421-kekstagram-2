c2f19f53afd4a1ef9a5125ae003cda251edbedb1
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-08-27 09:20:28 +0300

    Fix a bug

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ let currentCommentId = 0;
 const createComment = () => ({
   id: ++currentCommentId,
   avatar: `img/avatar-${avatarNumber}.svg`,
-  messages: `${MESSAGES[messageIndex]}. ${MESSAGES[nameIndex]}`,
+  messages: `${MESSAGES[messageIndex]}. ${MESSAGES[messageIndex]}`,
   name: `${NAMES[nameIndex]}`,
 });
 
