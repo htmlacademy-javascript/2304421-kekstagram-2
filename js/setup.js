9907d9a874dcc1e43f669fdcfcb6086d7ac337a8
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-08-27 20:43:19 +0300

    Divides the project into modules

---
+++ js/setup.js
@@ -0,0 +1,43 @@
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо.',
+  'Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+
+const NAMES = [
+  'Иван',
+  'Хуан',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+  'Сергей',
+  'Себастьян',
+  'Роза',
+  'Василиса'
+];
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+const FIRST_MESSAGE_INDEX = 0;
+const LAST_MESSAGE_INDEX = MESSAGES.length - 1;
+const FIRST_NAME_INDEX = 0;
+const LAST_NAME_INDEX = NAMES.length - 1;
+const MIN_COMMENTS_NUMBER = 0;
+const MAX_COMMENT_NUMBER = 30;
+
+const avatarNumber = getRandomInteger(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER);
+const messageIndex = getRandomInteger(FIRST_MESSAGE_INDEX, LAST_MESSAGE_INDEX);
+const nameIndex = getRandomInteger(FIRST_NAME_INDEX, LAST_NAME_INDEX);
+const likesNumber = getRandomInteger(MIN_LIKES, MAX_LIKES);
+const commentsNumber = getRandomInteger(MIN_COMMENTS_NUMBER, MAX_COMMENT_NUMBER);
