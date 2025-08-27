e721cd9665a186ccae66980bcf5b70d9a60b2328
Keks <keks@htmlacademy.ru>
2025-08-27 14:38:14 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,35 @@
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+
+}
+
+function isPalindrom(string) {
+  const normalizedString = string.replaceAll(' ', '').toUpperCase();
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return reversedString === normalizedString;
+}
+
+function getInteger(string) {
+  string = string.toString();
+  let integer = '';
+  for (let i = 0; i < string.length; i++) {
+    const parsed = parseInt(string[i], 10);
+    if (Number.isNaN(string[i]) !== true) {
+      integer += parsed;
+    }
+  }
+  return integer.length > 0 ? parseInt(integer, 10) : NaN;
+}const NAMES = [
+  'Иван',
+  'Хуан',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
