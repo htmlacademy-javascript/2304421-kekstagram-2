84541ef645f2960c70567b0a23b73fc40ad16488
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-09-19 16:20:45 +0300

    Finish module9-task2

--- js/utils.js
+++ js/utils.js
@@ -75,31 +75,31 @@ const isTextDescriptionValid = (value) => {
 };
 
 
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
+// function checkStringLength(string, maxLength) {
+//   return string.length <= maxLength;
 
-}
+// }
 
-function isPalindrom(string) {
-  const normalizedString = string.replaceAll(' ', '').toUpperCase();
-  let reversedString = '';
-  for (let i = normalizedString.length - 1; i >= 0; i--) {
-    reversedString += normalizedString[i];
-  }
-  return reversedString === normalizedString;
-}
+// function isPalindrom(string) {
+//   const normalizedString = string.replaceAll(' ', '').toUpperCase();
+//   let reversedString = '';
+//   for (let i = normalizedString.length - 1; i >= 0; i--) {
+//     reversedString += normalizedString[i];
+//   }
+//   return reversedString === normalizedString;
+// }
 
-function getInteger(string) {
-  string = string.toString();
-  let integer = '';
-  for (let i = 0; i < string.length; i++) {
-    const parsed = parseInt(string[i], 10);
-    if (Number.isNaN(string[i]) !== true) {
-      integer += parsed;
-    }
-  }
-  return integer.length > 0 ? parseInt(integer, 10) : NaN;
-}
+// function getInteger(string) {
+//   string = string.toString();
+//   let integer = '';
+//   for (let i = 0; i < string.length; i++) {
+//     const parsed = parseInt(string[i], 10);
+//     if (Number.isNaN(string[i]) !== true) {
+//       integer += parsed;
+//     }
+//   }
+//   return integer.length > 0 ? parseInt(integer, 10) : NaN;
+// }
 
 
 export { getRandomInteger };
