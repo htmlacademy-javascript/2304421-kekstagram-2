1f356b7439c33914365520348f2845140c02f73e
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-10-02 22:46:20 +0300

    Add filter module

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -3,11 +3,13 @@ import { showErrorMessage } from './utils.js';
 
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
+// const filterSection = document.querySelector('.img-filters');
 
 let photos = [];
 
 const renderThumbnails = (photoArray) => {
   photos = photoArray;
+  container.querySelectorAll('.picture').forEach((el) => el.remove());
   const fragment = document.createDocumentFragment();
 
   photoArray.forEach((photo) => {
@@ -23,13 +25,14 @@ const renderThumbnails = (photoArray) => {
   container.appendChild(fragment);
 };
 
-getData()
-  .then((data) => {
-    renderThumbnails(data);
-  })
-  .catch((err) => {
-    console.error(err.message);
-    showErrorMessage();
-  });
+// getData()
+//   .then((data) => {
+//     renderThumbnails(data);
+//     filterSection.classList.remove('img-filters--inactive');
+//   })
+//   .catch((err) => {
+//     console.error(err.message);
+//     showErrorMessage();
+//   });
 
-export { container, photos };
+export { container, photos, renderThumbnails };
